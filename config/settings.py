308996badad52dac73f6f2bcbdeff10d0424ef17
andrew <j_k7@bk.ru>
2022-10-10 20:46:10 +0300

    add STATIC_ROOT and add static in .gitignore

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # static, media
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR / 'template_static',)
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
